e6fb2941ae1e38981fffee8eca4ef55cea5080cc
Jeremy Rome <jeremy.rome93@gmail.com>
2024-05-14 16:10:17 +0200

    Solved the adding a task challenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,25 @@
 class TasksController < ApplicationController
-  def all
+  def index
     @tasks = Task.all
   end
 
-  def find
+  def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task_new = Task.new(task_params)
+    @task_new.save
+    redirect_to tasks_path
+  end
+
+  private
+
+def task_params
+  params.require(:task).permit(:title, :details)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "all", to: "tasks#all"
-  get "tasks/:id", to: 'tasks#find'
+
+  get "tasks", to: "tasks#index"
+  
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  get "tasks/:id", to: 'tasks#show'
+
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
